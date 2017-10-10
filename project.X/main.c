94c58d6c81cdeb3a2179ad5eb455f95f44c750b6
Nomrom <mccdahl@gmail.com>
2017-10-10 13:19:17 -0600

    Added main file
    has debugging pragma

---
+++ project.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: mccda
+ *
+ * Created on October 10, 2017, 1:18 PM
+ */
+
+#pragma config ICS = PGx3 //this allows debugging
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
