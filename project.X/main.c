e2d44fdc651f87b86179b1958d66067975e29e96
Nomrom <mccdahl@gmail.com>
2017-10-10 13:27:16 -0600

    Debugging tools from today's lesson

--- project.X/main.c
+++ project.X/main.c
@@ -6,8 +6,16 @@
  */
 
 #pragma config ICS = PGx3 //this allows debugging
+#pragma config FNOSC = FRCDIV // select 8MHz oscillator with postscaler
+
 #include "xc.h"
+#include "libpic30.h" //include library for _delay_ms() function)
 
 int main(void) {
+    
+    _RCDIV = 0b010; // postscale oscillator (divide-by-4))
+
+    Nop();  //do nothing cycle for one-instruction
+    
     return 0;
 }
