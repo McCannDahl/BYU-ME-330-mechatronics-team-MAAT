5f44c384d4686e66cf6624dbb81c0edd62b623e7
Nomrom <mccdahl@gmail.com>
2017-10-26 12:55:39 -0600

    .
    .

--- project.X/main.c
+++ project.X/main.c
@@ -5,7 +5,7 @@
  * Created on October 10, 2017, 1:18 PM
  */
 
-#pragma config ICS = PGx3 //this allows debugging
+#pragma config ICS = PGx3 //this allows debugging.... use PGC/D pair 3 (pins 9,10) for debugging
 #pragma config FNOSC = FRCDIV // select 8MHz oscillator with postscaler
 
 #include "xc.h"
@@ -16,6 +16,9 @@ int main(void) {
     _RCDIV = 0b010; // postscale oscillator (divide-by-4))
 
     Nop();  //do nothing cycle for one-instruction
+    
+    while(1){
+    }
     
     return 0;
 }
